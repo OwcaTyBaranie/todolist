f004bf591ee3d769e3e0ac29990d3e61418b27aa
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-03-18 01:36:46 +0100

    Finish app

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,23 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import Button from '../Button/Button';
+
+const ColumnForm = props => {
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title, icon: icon});
+        setTitle('');
+        setIcon('');
+    };
+
+	return (
+        <form className={styles.ColumnForm} onSubmit={handleSubmit}>
+        <span>Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} /></span>
+        <span> Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} /></span>
+        <Button>Add column</Button>
+</form>
+	);
+};
+export default ColumnForm;
