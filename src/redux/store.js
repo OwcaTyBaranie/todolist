a6ce87b98e82b2934d17ea14156745148c79ff23
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-06-26 22:59:11 +0200

    Change if function to switch in reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,19 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
+import shortid from 'shortid';
+
+// const reducer = (state, payload) => {
+//   if(payload.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, { ...payload.newColumn, id: shortid() }]};
+//   return state;
+// };
 
 const reducer = (state, action) => {
+  switch(action.type){
+   case 'ADD_COLUMN':
+   return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
+   default:
   return state;
+  }
 };
 
 const store = createStore(
